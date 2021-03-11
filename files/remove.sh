b60e28ab771cb46fa02f8a7330297fb9fd88c316
MattDierick <matt262810@gmail.com>
2021-03-11 14:22:52 +0100

    fix remove

--- files/remove.sh
+++ files/remove.sh
@@ -5,12 +5,12 @@
 curl -sk -c cookie.txt -X POST --url 'https://'$CTRL_FQDN'/api/v1/platform/login' --header 'Content-Type: application/json' --data '{"credentials": {"type": "BASIC","username": "'"$CTRL_USERNAME"'","password": "'"$CTRL_PASSWORD"'"}}'
 
 # 1. Remove the instance from gateway
-curl -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' --url 'https://'$CTRL_FQDN'/api/v1/services/environments/'$LOCATION'/gateways/'$GATEWAY -o update.json
+curl -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' --url 'https://'$CTRL_FQDN'/api/v1/services/environments/'$SERVICE'/gateways/'$GATEWAY -o update.json
 jq '.desiredState.ingress.placement.instanceRefs -= [{"ref": "/infrastructure/locations/'$LOCATION'/instances/'$HOSTNAME'"}]' update.json > $HOSTNAME.json
 
 # cat $HOSTNAME.json
 
-curl -sk -b cookie.txt -c cookie.txt -X PUT -d @$HOSTNAME.json --header 'Content-Type: application/json' --url 'https://'$CTRL_FQDN'/api/v1/services/environments/'$LOCATION'/gateways/'$GATEWAY
+curl -sk -b cookie.txt -c cookie.txt -X PUT -d @$HOSTNAME.json --header 'Content-Type: application/json' --url 'https://'$CTRL_FQDN'/api/v1/services/environments/'$SERVICE'/gateways/'$GATEWAY
 
 # 2. Remove the instance from infrastructure
 curl -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' -X DELETE --url 'https://'$CTRL_FQDN'/api/v1/infrastructure/locations/'$LOCATION'/instances/'$HOSTNAME
